bbd06561e69907dc778430f3b4e96bf864c6f7e6
Mysario <dima_mielnichienko@mail.ru>
2022-02-12 18:58:56 +0300

    UploadFiles
    Client/Server Files

---
+++ Main/Client/Client.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <algorithm>
+#include <mutex>
+#include <cmath>
+#pragma comment(lib,"ws2_32.lib")
+#pragma warning(disable:4996)
+#include <WinSock2.h>
+
+std::mutex Switcher;
+char* Insert(int &SizeOfArray, char Array[],char Element,int Position){
+	int Index;
+
+	for (Index = SizeOfArray; Index >= Position; Index--)
+		Array[Index + 1] = Array[Index];
+
+	Array[Position] = Element;
+	SizeOfArray++;
+	return Array;
+}
+char* GetSizeOfInt(int &Size,int num) {
+	Size = 0;
+	int NumBuf=num;
+	while (NumBuf) {
+		NumBuf /= 10;
+		Size++;
+	}
+	char* Array = new char[Size];
+	Array[0] = num / (int)pow(10, Size - 1);
+	std::cout << (int)Array[0];
+	for (int i = 1; i < Size; i++) {
+		Array[i] = num / (int)pow(10, Size - 1 - i) - (int)Array[i-1] * 10;
+		std::cout << (int)Array[i];
+		num -= (int)Array[i-1] * pow(10, Size - 1);
+	}
+	return Array;
+}
+class Chat {
+private:
+	
+public:
+	char Buffer[128];
+	void GetWord(char Word[128]) {
+		Switcher.lock();
+		std::cin >> Word;
+		int Size = strlen(Word);
+		if (Size > 64) {
+			std::cout << "Long Word!" << std::endl;
+			return;
+		}
+
+		for (int Index = 0; Index < Size;Index++) {
+				if (('9' < Word[Index]) || (Word[Index] < '0')) {
+					std::cout << "BadWord" <<std::endl;
+					return;
+				}
+		}
+
+		std::sort(Word,Word+Size);
+		for (int Index = 0; Index < Size; Index++) {
+			if (Word[Index]%2 == 0) {
+				Word[Index] = 'K';
+				Insert(Size, Word, 'B', ++Index);
+			}
+		}
+		//for (int Index = 0; Index < Size; Index++) {
+		//	std::cout << Word[Index] << std::endl;
+		//}
+		Switcher.unlock();
+		return;
+	}
+	void SendWord(char Word[128], SOCKET Connection) {
+		Switcher.lock();
+		int Size = strlen(Word),sum=0;
+		for (int Index = 0; Index < Size; Index++) {
+			if (('9' >= Word[Index]) && (Word[Index] >= '0')) {
+				sum += Word[Index] - '0';
+				//std::cout << "Element= " << Word[Index] - '0' << std::endl;
+			}
+		}
+		std::cout << "Sum of Elements= " << sum << std::endl;
+		//Insert(Size, Word, Size, 0);
+		//char Message[]=Size;
+		//send(Connection,Message,1, NULL);
+		Word = NULL;
+		Switcher.unlock();
+		
+		return;
+	}
+};
+
+int main()
+{	
+	int c = 1;
+	GetSizeOfInt(c, 13223);
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		std::cout << "Eror lib " << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int SizeOfAddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, SizeOfAddr) != 0) {
+		std::cout << "Error Connect to Server" << std::endl;
+		return 1;
+	}
+	std::cout << "Connect to Server" << std::endl;
+	
+	Chat Client;
+	for (;;) {
+	std::thread FirstT(&Chat::GetWord, Client,Client.Buffer);
+	if (FirstT.joinable())
+		FirstT.join();
+	//std::cout << Client.Buffer[0] << std::endl;
+	std::thread SecondT(&Chat::SendWord, Client, Client.Buffer,Connection);
+	if (SecondT.joinable())
+		SecondT.join();
+	}
+
+	system("pause");
+
+
+	return 0;
+}
+
---
+++ Main/Server/Server.cpp
@@ -0,0 +1,46 @@
+﻿#pragma comment(lib,"ws2_32.lib")
+#include <iostream>
+#include <WinSock2.h>
+#pragma warning(disable:4996)
+
+int main(){
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		std::cout << "Eror lib " << std::endl;
+		exit(1);
+	}
+	
+	SOCKADDR_IN addr;
+	int SizeOfAddr = sizeof(addr);
+	addr.sin_addr.s_addr= inet_addr("127.0.0.1");
+	addr.sin_port=htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET SocketListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(SocketListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(SocketListen, SOMAXCONN);
+
+	SOCKET NewConnect;
+	NewConnect = accept(SocketListen, (SOCKADDR*)&addr, &SizeOfAddr);
+
+	if (NewConnect == 0) {
+		std::cout << "Eror Connect" << std::endl;
+	}
+	else {
+		std::cout << "Connected!!" << std::endl;
+	}
+	char Buffer[128];
+	for (;;) {
+		recv(NewConnect, Buffer, sizeof(Buffer), NULL);
+		int Size = strlen(Buffer);
+
+		std::cout << "Size= " << (int)Buffer[0] << std::endl;
+		//std::cout << "Msg from client= "<< std::endl;
+		for (int Index = 1; Index <= Buffer[0]; Index++) {
+			std::cout << Buffer[Index];
+		}
+		std::cout << std::endl;
+	}
+	system("pause");
+}
